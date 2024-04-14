bdeef3a5f0cb999cf561a1ef197729ec99159219
Elyor Usmanov <94918957+masterusmanov@users.noreply.github.com>
2024-04-14 19:48:05 +0500

    update

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,23 +1,23 @@
-import { NavLink, useRouteError } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import errors from "../../assets/images/error.webp";
 
 export default function ErrorPage(){
-    const error = useRouteError();
-    console.log(error);
-    if(error.status === 404){
-        return(
-            <div className="">
-                <img src={errors} alt="" className="mt-[200px] mx-auto w-[500px]"/>
+    // const error = useRouteError();
+    // console.log(error);
+    // if(error.status === 404){
+    //     return(
+    //         <div className="">
+    //             <img src={errors} alt="" className="mt-[200px] mx-auto w-[500px]"/>
     
-                <div className="">
-                    <h1 className="text-[20px] text-blue-600 font-[700] text-center">Bu sahifa topilmadi. Bosh sahifaga qayting!</h1>
-                </div>
-                <div className="w-[500px] mx-auto mt-[50px]">
-                    <NavLink to="/" className="bg-blue-500 ml-[90px] md:ml-[150px] xl:ml-[150px] text-white py-4 px-8 rounded-full ml">Bosh sahifaga qaytish</NavLink>
-                </div>
-            </div>
-        )
-    }
+    //             <div className="">
+    //                 <h1 className="text-[20px] text-blue-600 font-[700] text-center">Bu sahifa topilmadi. Bosh sahifaga qayting!</h1>
+    //             </div>
+    //             <div className="w-[500px] mx-auto mt-[50px]">
+    //                 <NavLink to="/" className="bg-blue-500 ml-[90px] md:ml-[150px] xl:ml-[150px] text-white py-4 px-8 rounded-full ml">Bosh sahifaga qaytish</NavLink>
+    //             </div>
+    //         </div>
+    //     )
+    // }
     return(
         <div className="">
             <img src={errors} alt="" className="mt-[200px] mx-auto w-[500px]"/>
