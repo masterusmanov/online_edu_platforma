dca45dc1f2cfab2d2b3490dd4ad798c97833db3d
Elyor Usmanov <94918957+masterusmanov@users.noreply.github.com>
2024-04-21 08:03:55 +0500

    update

--- src/Pages/Main/Main.jsx
+++ src/Pages/Main/Main.jsx
@@ -4,6 +4,7 @@ import foundation from '../../assets/images/Main/foundation.jpg';
 import react from '../../assets/images/Main/reactjs.png';
 import python from '../../assets/images/Main/django.jpg';
 import design from '../../assets/images/Main/design.png';
+import { NavLink } from "react-router-dom";
 
 
 function Monitoring() {
@@ -39,7 +40,7 @@ function Monitoring() {
             <div className="bg-white h-[50px] flex items-center">
                 <h1 className=" px-5 md:px-10 py-5">Bosh sahifa</h1>
             </div>
-            <div className="flex gap-4">
+            <div className="md:flex gap-4">
                     <div className="md:w-[70%] m-5 md:ml-5 md:my-5 p-5 bg-white  rounded-md xl:grid gap-4">
                         <div className="grid gap-2 lg:flex  md:gap-0 items-center justify-between text-[14px] lg:text-[12px]">
                             <div>
@@ -74,10 +75,15 @@ function Monitoring() {
                             <div className="grid gap-1">
                                 <h1 className="text-[10px]">15% videodars yakunlangan</h1>
                                 <div className="w-full h-[5px] rounded-full bg-neutral-200 dark:bg-neutral-600">
-                                    <div className="h-[5px] bg-blue-500 rounded-full" style={{ width: "15%" }}></div>
+                                    <div className="h-[5px] bg-blue-500 rounded-full text-white" style={{ width: "15%" }}></div>
                                 </div>
                             </div>
                         </div>
+                    </div>
+                    <div className="md:w-[70%] m-5 md:ml-5 md:my-5 p-5 bg-white  rounded-md xl:grid gap-4">
+                        <h1 className="font-[700] text-[24px]">Assalomu alaykum UserName! <br /> Xush kelibsiz!</h1>
+                        <p>Sizda hozirda hech qanday kurs mavjud emas, “Barcha kurslar” bo‘limidan o‘zingizga ma’qul kursni xarid qilishingiz mumkin</p>
+                        <NavLink to="#" className="py-1 px-3 bg-blue-500">Barcha kurslar bo`limiga o`tish</NavLink>
                     </div>
                 <div className=" w-[30%] my-5 mr-2 p-3 hidden md:grid bg-white gap-4 rounded-md">
                     <div className="grid gap-3 lg:gap-2 bg-[#F7F7F5] p-2 rounded-lg">
