ce2ddb16c0ec5643525337a9a9d65e3a4969ebf3
Elyor Usmanov <94918957+masterusmanov@users.noreply.github.com>
2024-04-30 23:25:29 +0500

    update

--- src/Components/Navbar/Comment.jsx
+++ src/Components/Navbar/Comment.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
+import image from "../../assets/images/navbar/account.png"
 
 const Comment = () => {
 
     const cards = [
         {
             id: 1,
-            title: "Kirish",
+            name: "Name Surname",
             notes: "Lorem ipsum dolor sit amet.",
+            clock: "15:00",
+            times: "01.05.2024",
+            img: image
         },
         {
             id: 2,
-            title: "Kirish",
-            notes: "Lorem ipsum dolor sit amet."
+            name: "Name Surname",
+            notes: "Lorem ipsum dolor sit amet.",
+            clock: "15:00",
+            times: "01.05.2024",
+            img: image
         }
     ]
   
@@ -24,13 +31,16 @@ const Comment = () => {
                 // eslint-disable-next-line react/jsx-key
                     <>
                     <div className='flex justify-between items-center'>
-                        <div>
-                            <h1 className='text-[14px] font-[700]'>{item.id}-dars: {item.title}</h1>
-                            <p>{item.notes}</p>
+                        <div className='flex items-center gap-4'>
+                            <img src={item.img} alt="account" className='opacity-40 w-[40px]'/>
+                            <div>
+                                <h1 className='text-[14px] font-[700]'>{item.name}<span className='text-gray-500'> - {item.clock} {item.times}</span></h1>
+                                <p>{item.notes}</p>
+                            </div>
                         </div>
                         <div className='flex items-center gap-6 text-[20px] text-gray-500'>
-                            <i className='bx bx-edit-alt text-blue-600 flex items-center'><span className='text-[14px] pl-2'>Tahrirlash</span></i>
-                            <i className='bx bx-trash text-red-600 flex items-center'><span className='text-[14px] pl-2'>O`chirish</span></i>
+                            <i className='bx bx-edit-alt text-blue-600 flex items-center cursor-pointer'><span className='text-[14px] pl-2'>Tahrirlash</span></i>
+                            <i className='bx bx-trash text-red-600 flex items-center cursor-pointer'><span className='text-[14px] pl-2'>O`chirish</span></i>
                         </div>
                     </div></>
             ))}
