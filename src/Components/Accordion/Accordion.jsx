367f0a06e333d6802eae92101172b29f98f07717
Elyor Usmanov <94918957+masterusmanov@users.noreply.github.com>
2024-05-02 17:08:17 +0500

    update

--- src/Components/Accordion/Accordion.jsx
+++ src/Components/Accordion/Accordion.jsx
@@ -30,13 +30,18 @@ const Accordion = ({ items }) => {
             <span className='text-[14px] text-[#77BF44]'>{activeIndex === index ? '▲' : '▼'}</span>
           </div>
           {activeIndex === index && (
-            <div className="p-4 flex items-center gap-4">
-                {isViewed ? (
-                    <p role="img" aria-label="viewed" style={{ fontSize: '16px' }}><i className='bx bx-check bg-[#77BF44] p-1 rounded-full text-white'></i></p>
-                ) : (
-                    <p role="img" aria-label="not-viewed" style={{ fontSize: '16px' }}><i className='bx bx-play-circle bg-gray-200 p-1 rounded-full'></i></p>
-                )}
-              <span className='text-[14px]'>{item.content}</span>
+            <div>
+                {item.videos.map((el) => (        
+                // eslint-disable-next-line react/jsx-key
+                <div className="p-4 flex items-center gap-4">
+                    {isViewed ? (
+                        <p role="img" aria-label="viewed" style={{ fontSize: '16px' }}><i className='bx bx-check bg-[#77BF44] p-1 rounded-full text-white'></i></p>
+                    ) : (
+                        <p role="img" aria-label="not-viewed" style={{ fontSize: '16px' }}><i className='bx bx-play-circle bg-gray-200 p-1 rounded-full'></i></p>
+                    )}
+                    <h2 className='text-[14px]'>{el.contentone}</h2>
+                </div>
+            ))}
             </div>
           )}
         </div>
--- src/Pages/Theme/Theme.jsx
+++ src/Pages/Theme/Theme.jsx
@@ -12,37 +12,194 @@ const items = [
     {
         id: 1, 
         title: 'Kirish',
-        content: 'Kirish',
+        videos: [
+            {
+                id: 1,
+                contentone: "Kirish",
+                videotime: localStorage.getItem("videotime")
+            },
+            {
+                id: 2,
+                contentone: "Yuzaga kelishi mumkin bo'lgan muammolar",
+                videotime: localStorage.getItem("videotime")
+            }
+        ]
     },
     {
         id: 2, 
         title: 'Kompyuter',
-        content: 'Kompyuter nimalardan iborat',
+        videos: [
+            {
+                id: 1,
+                contentone: "Kompyuter nimalardan iborat",
+                videotime: localStorage.getItem("videotime")
+            },
+            {
+                id: 2,
+                contentone: "Vazifalar paneli",
+                videotime: localStorage.getItem("videotime")
+            },
+            {
+                id: 3,
+                contentone: "Ish stoli",
+                videotime: localStorage.getItem("videotime")
+            },
+            {
+                id: 4,
+                contentone: '"Mening kompyuterim" bo`limi',
+                videotime: localStorage.getItem("videotime")
+            },
+            {
+                id: 5,
+                contentone: "Ilovalar",
+                videotime: localStorage.getItem("videotime")
+            }
+        ]
     },
     {
         id: 3, 
         title: 'Brouzerdan samarali foydalanish',
-        content: 'Chrome brauzeri | Disk',
+        videos: [
+            {
+                id: 1,
+                contentone: "Chrome brauzeri | Disk",
+                videotime: localStorage.getItem("videotime")
+            },
+            {
+                id: 2,
+                contentone: "Brauzer qo'shimchalari",
+                videotime: localStorage.getItem("videotime")
+            },
+        ]
     },
     {
         id: 4, 
         title: 'Ofice ilovalaridan foydalanish',
-        content: 'Google docs | matn tahrirchisi',
+        videos: [
+            {
+                id: 1,
+                contentone: "Google docs | matn tahrirchisi",
+                videotime: localStorage.getItem("videotime")
+            },
+            {
+                id: 2,
+                contentone: "Jadavallar bilan ishlash: Google sheets [1-qism]",
+                videotime: localStorage.getItem("videotime")
+            },
+            {
+                id: 3,
+                contentone: "Jadavallar bilan ishlash: Google sheets [2-qism]",
+                videotime: localStorage.getItem("videotime")
+            },
+            {
+                id: 4,
+                contentone: "Jadavallar bilan ishlash: Google sheets [4-qism]",
+                videotime: localStorage.getItem("videotime")
+            },
+            {
+                id: 5,
+                contentone: "Jadavallar bilan ishlash: Google sheets [5-qism]",
+                videotime: localStorage.getItem("videotime")
+            },
+            {
+                id: 6,
+                contentone: "Google forms [1-qism]",
+                videotime: localStorage.getItem("videotime")
+            },
+            {
+                id: 7,
+                contentone: "Google forms [2-qism]",
+                videotime: localStorage.getItem("videotime")
+            },
+            {
+                id: 8,
+                contentone: "Google slides",
+                videotime: localStorage.getItem("videotime")
+            }
+        ]
     },
     {
         id: 5, 
         title: 'Telegramdan samarali foydalanish',
-        content: 'Asosiy sozlamalar [1-qism]',
+        videos: [
+            {
+                id: 1,
+                contentone: "Asosiy sozlamalar [1-qism]",
+                videotime: localStorage.getItem("videotime")
+            },
+            {
+                id: 2,
+                contentone: "Asosiy sozlamalar [2-qism]",
+                videotime: localStorage.getItem("videotime")
+            },
+            {
+                id: 3,
+                contentone: "Guruh bilan ishlash",
+                videotime: localStorage.getItem("videotime")
+            },
+            {
+                id: 4,
+                contentone: "Muhim qoidalar",
+                videotime: localStorage.getItem("videotime")
+            },
+            {
+                id: 5,
+                contentone: "Kanal bilan ishlash",
+                videotime: localStorage.getItem("videotime")
+            },
+            {
+                id: 6,
+                contentone: "Layfhaklar",
+                videotime: localStorage.getItem("videotime")
+            },
+            {
+                id: 7,
+                contentone: "Botlar bilan ishlash [1-qism]",
+                videotime: localStorage.getItem("videotime")
+            },
+            {
+                id: 8,
+                contentone: "Botlar bilan ishlash [2-qism]",
+                videotime: localStorage.getItem("videotime")
+            }
+        ]
     },
     {
         id: 6, 
         title: 'Foydali saytlar bilan ishlash',
-        content: 'Telegra.ph sayti bilan ishlash',
+        videos: [
+            {
+                id: 1,
+                contentone: "Telegra.ph sayti bilan ishlash",
+                videotime: localStorage.getItem("videotime")
+            },
+            {
+                id: 2,
+                contentone: "Canva.com sayti bilan ishlash",
+                videotime: localStorage.getItem("videotime")
+            },
+            {
+                id: 3,
+                contentone: "Matnlar; tez yozish",
+                videotime: localStorage.getItem("videotime")
+            },
+            {
+                id: 4,
+                contentone: "Rasmlar bilan ishlash",
+                videotime: localStorage.getItem("videotime")
+            }
+        ]
     },
     {
         id: 7, 
         title: 'Tez kunda',
-        content: 'Darslar soni 0ta Mavzu vaqti 0:00',
+        videos: [
+            {
+                id: 1,
+                contentone: "Darslar soni 0ta Mavzu vaqti",
+                videotime: localStorage.getItem("videotime")
+            }
+        ]
     },
   ];
 
@@ -111,7 +268,7 @@ export default function Theme(){
                     </div>
                     <div className="p-5 my-5 lg:my-0 lg:w-[350px] bg-white lg:hidden">
                         <h1 className='text-[#77BF44] font-[700] text-[16px]'>Kompyuter savodxonligi darslari</h1>
-                        <p className="text-[13px] font-[600] my-2">Mavzular soni: Nta </p>
+                        <p className="text-[13px] font-[600] my-2 text-gray-400">Mavzular soni: Nta </p>
                         <div>
                             <Accordion items={items} />
                         </div>
